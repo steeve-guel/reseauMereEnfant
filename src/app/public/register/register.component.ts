df190f0650ec2aa5ba7067d1fae2779bfd9409ce
krafty2 <steevezguel@gmail.com>
2024-04-03 10:33:57 +0000

    Feat update register

--- src/app/public/register/register.component.ts
+++ src/app/public/register/register.component.ts
@@ -3,11 +3,19 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule,MatButtonModule,RouterLink],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    RouterLink,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
