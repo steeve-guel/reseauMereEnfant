630178cdfb51d2ac94143bc2999b565fa5a52923
krafty2 <steevezguel@gmail.com>
2024-08-16 16:00:41 +0000

    Update header soumission page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { JourneeRemehbsComponent } from './public/journee-remehbs/journee-remehb
 import { RemehbsComponent } from './public/remehbs/remehbs.component';
 import { LoginComponent } from './public/login/login.component';
 import { SoumettreFormComponent } from './participant/soumettre-form/soumettre-form.component';
+import { FeedbackComponent } from './public/feedback/feedback.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'remehbs', pathMatch: 'full' }, //redirection vers
@@ -16,6 +17,7 @@ export const routes: Routes = [
       { path: 'accueil', component: AccueilComponent },
       { path: 'journee-remehbs', component: JourneeRemehbsComponent },
       { path: 'soummettre', component: SoumettreFormComponent },
+      { path: 'feedback', component: FeedbackComponent }
     ],
   },
   { path: 'remehbs-register', component: RegisterComponent },
